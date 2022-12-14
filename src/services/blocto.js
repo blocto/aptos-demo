aea11243986eff99e39ddb96fb7445c33a9abcf5
Hsuan Lee <boczeratul@gmail.com>
2022-12-14 23:52:13 +0800

    Add comment to network number

--- src/services/blocto.js
+++ src/services/blocto.js
@@ -2,7 +2,7 @@ import BloctoSDK from '@blocto/sdk';
 
 const bloctoSDK = new BloctoSDK({
   aptos: {
-    chainId: 2,
+    chainId: 2, // 1 for mainnet, 2 for testnet
   },
 
   appId: '93755015-b89f-48a5-bffe-c25cc5708386',
